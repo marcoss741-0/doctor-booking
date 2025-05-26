bdc6f448c2f09c9e2ae2615e7cfc400c341023c1
MARCOS MARTINS DA SILVA SANTOS <marcossan741@icloud.com>
2025-05-26 12:00:34 -0300

    chore: add shadcn in project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="">
+    <div className="p-5">
       <h1>Home Page</h1>
+      <Button className="mt-4" onClick={() => alert("Button clicked!")}>
+        Click Me
+      </Button>
     </div>
   );
 }
