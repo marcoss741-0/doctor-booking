1a9906411b46a74ca9636cf3e1ad603e813403ca
MARCOS MARTINS DA SILVA SANTOS <marcossan741@icloud.com>
2025-05-31 18:44:05 -0300

    feat: update AppSidebar with new menu items and logout functionality
    - Replaced existing menu items with new ones for Dashboard, Appointments, Patients, and Doctors.
    - Added a logo to the sidebar header.
    - Implemented user session management and logout functionality using authClient.
    - Introduced a dropdown menu for user actions in the sidebar footer.
    
    feat: add logo.svg to public assets
    
    - Added a new logo.svg file for branding purposes.
    
    feat: implement createClinic action
    
    - Created a new server action to handle clinic creation.
    - Integrated user session validation and clinic-user association.
    
    feat: create Avatar component for user profile images
    
    - Developed Avatar, AvatarImage, and AvatarFallback components for user profile representation.
    
    feat: implement DropdownMenu component
    
    - Created a reusable DropdownMenu component with various subcomponents for dropdown functionality.

--- src/app/(protected)/_components/app-sidebar.tsx
+++ src/app/(protected)/_components/app-sidebar.tsx
@@ -1,60 +1,92 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+"use client";
+
+import {
+  CalendarDays,
+  LayoutDashboard,
+  LogOutIcon,
+  Stethoscope,
+  Users2Icon,
+} from "lucide-react";
 
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/_components/ui/sidebar";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/_components/ui/dropdown-menu";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
-// Menu items.
 const items = [
   {
-    title: "Home",
-    url: "#",
-    icon: Home,
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
   },
   {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
+    title: "Appointments",
+    url: "/appointments",
+    icon: CalendarDays,
   },
   {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
+    title: "Pacientes",
+    url: "/patients",
+    icon: Users2Icon,
   },
   {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
+    title: "Médicos",
+    url: "/doctors",
+    icon: Stethoscope,
   },
 ];
 
 export function AppSidebar() {
+  const session = authClient.useSession();
+  const router = useRouter();
+  async function handleLogout() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/authentication");
+        },
+      },
+    });
+  }
+
   return (
     <Sidebar>
       <SidebarContent>
+        <SidebarHeader className="border-b p-4">
+          <Image src="/logo.svg" alt="logo" width={136} height={32} />
+        </SidebarHeader>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupLabel>Menu principal</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
+                <SidebarMenuItem key={item.title} className="space-y-2">
                   <SidebarMenuButton asChild>
-                    <a href={item.url}>
+                    <Link
+                      href={item.url}
+                      className="flex items-center gap-2 text-[16px]"
+                    >
                       <item.icon />
                       <span>{item.title}</span>
-                    </a>
+                    </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
@@ -62,6 +94,39 @@ export function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <div className="flex cursor-pointer gap-4 rounded-md border p-4">
+                    {/* <Avatar className="h-8 w-8 rounded-lg grayscale">
+                    <AvatarImage
+                      src={session?.user.image || "icons8-user-48.png"}
+                      alt={session?.user.name}
+                    />
+                    <AvatarFallback className="rounded-lg">FA</AvatarFallback>
+                  </Avatar> */}
+                    <div className="grid flex-1 items-center text-left text-sm leading-tight">
+                      <span className="truncate font-medium"></span>
+                    </div>
+                  </div>
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem
+                  className="cursor-pointer"
+                  onClick={handleLogout}
+                >
+                  <LogOutIcon />
+                  <span>Sair</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 }
